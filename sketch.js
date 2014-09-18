7f9ed577cf20b975ea38cb56b78e928a847ee38d
dnjs0228 <dnjsrud0228@naver.com>
2014-09-18 13:13:58 +0900

    new

--- sketch.js
+++ sketch.js
@@ -1,18 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
 
-  var x;
+}
 
-  x = 100;
-  ellipse(x, 100, 50, 50);
+function draw () {
 
-  x = 250;
-  ellipse(x, 100, 50, 50);
+   background(128, 0, 64);
 
-  x = 300;
-  ellipse(x, 100, 50, 50);
+for (var x = 1; x <= 8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+}
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
